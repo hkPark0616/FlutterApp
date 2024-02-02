c052402bac0ce95c9d40177f3d21636700a410fc
hkPark0616 <113004801+hkPark0616@users.noreply.github.com>
2024-02-02 15:13:57 +0900

    Update dbConnector.dart

--- lib/config/dbConnector.dart
+++ lib/config/dbConnector.dart
@@ -6,9 +6,9 @@ Future<MySQLConnection> dbConnector() async {
   final conn = await MySQLConnection.createConnection(
     host: "10.0.2.2",
     port: 3306,
-    userName: "root",
-    password: "rainbow@6861",
-    databaseName: "flutter", // optional
+    userName: "Your userName",
+    password: "Your password",
+    databaseName: "Your databaseName", // optional
   );
 
   await conn.connect();
